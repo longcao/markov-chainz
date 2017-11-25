59971fa92c53e4237b16a3338a5f6241f975e105
Long Cao <longcao@gmail.com>
2017-11-25 18:18:00 -0500

    only tweet once on boot

--- src/main/scala/markovchainz/Main.scala
+++ src/main/scala/markovchainz/Main.scala
@@ -1,55 +1,46 @@
 package markovchainz
 
 import java.io.File
-import java.util.concurrent._
 
 import twitter4j.conf.ConfigurationBuilder
 import twitter4j.{ Twitter, TwitterFactory }
 
-object Main extends App {
+object Main {
   import Markovchainz._
   import Markov._
 
-  val fileName = args(0)
+  def getEnvVar(name: String): String =
+    sys.env.getOrElse(name, throw new Exception(s"env var $name missing"))
+  
+  def main(args: Array[String]): Unit = {
+    val fileName = args(0)
 
-  def getEnvVar(name: String): String = sys.env.getOrElse(name, throw new Exception(s"env var $name missing"))
 
-  val twitterConsumerKey       = getEnvVar("twitterConsumerKey")
-  val twitterConsumerSecret    = getEnvVar("twitterConsumerSecret")
-  val twitterAccessToken       = getEnvVar("twitterAccessToken")
-  val twitterAccessTokenSecret = getEnvVar("twitterAccessTokenSecret")
+    val twitterConsumerKey       = getEnvVar("twitterConsumerKey")
+    val twitterConsumerSecret    = getEnvVar("twitterConsumerSecret")
+    val twitterAccessToken       = getEnvVar("twitterAccessToken")
+    val twitterAccessTokenSecret = getEnvVar("twitterAccessTokenSecret")
 
-  val cb = new ConfigurationBuilder()
-    .setOAuthConsumerKey(twitterConsumerKey)
-    .setOAuthConsumerSecret(twitterConsumerSecret)
-    .setOAuthAccessToken(twitterAccessToken)
-    .setOAuthAccessTokenSecret(twitterAccessTokenSecret)
+    val cb = new ConfigurationBuilder()
+      .setOAuthConsumerKey(twitterConsumerKey)
+      .setOAuthConsumerSecret(twitterConsumerSecret)
+      .setOAuthAccessToken(twitterAccessToken)
+      .setOAuthAccessTokenSecret(twitterAccessTokenSecret)
 
-  val twitter = new TwitterFactory(cb.build()).getInstance
+    val twitter = new TwitterFactory(cb.build()).getInstance
 
-  val megaMap = generateFullAffixMap(new File(fileName))
+    val megaMap = generateFullAffixMap(new File(fileName))
 
-  def postTweetsForever(twitter: Twitter, initialDelaySeconds: Int, periodSeconds: Int) = {
-    val ex = new ScheduledThreadPoolExecutor(1)
+    val chain = wordsToSentence(walkChain(megaMap))
 
-    val task = new Runnable {
-      def run = {
-        val chain = wordsToSentence(walkChain(megaMap))
+    try {
+      val status = twitter.updateStatus(chain)
 
-        try {
-          val status = twitter.updateStatus(chain)
-
-          println(s"Chain: $chain")
-          println(s"Posted status: ${status.getId}")
-          println()
-        } catch {
-          case ex: Exception => println(ex.getMessage)
-        }
-      }
+      println(s"Chain: $chain")
+      println(s"Posted status: ${status.getId}")
+      println()
+    } catch {
+      case ex: Exception => println(ex.getMessage)
     }
-
-    ex.scheduleAtFixedRate(task, initialDelaySeconds, periodSeconds, TimeUnit.SECONDS)
   }
-
-  postTweetsForever(twitter, 1, 12 * 60 * 60)
 }
